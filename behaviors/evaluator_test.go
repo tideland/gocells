f13554e0c7f4e40db1d10a783ed06a2d878f7a35
Frank Mueller <mue@tideland.biz>
2017-06-03 23:25:33 +0200

    Started with evaluator test

--- behaviors/evaluator_test.go
+++ behaviors/evaluator_test.go
@@ -12,10 +12,8 @@ package behaviors_test
 //--------------------
 
 import (
-	"context"
 	"strconv"
 	"testing"
-	"time"
 
 	"github.com/tideland/golib/audit"
 
@@ -32,38 +30,24 @@ import (
 // 6.0 for 3 times.
 func TestEvaluatorBehavior(t *testing.T) {
 	assert := audit.NewTestingAssertion(t, true)
+	sigc := audit.MakeSigChan()
 	generator := audit.NewGenerator(audit.FixedRand())
-	ctx := context.Background()
 	env := cells.NewEnvironment("evaluator-behavior")
 	defer env.Stop()
 
-	evaluate := func(event cells.Event) (float64, error) {
+	evaluator := func(event cells.Event) (float64, error) {
 		i, err := strconv.Atoi(event.Topic())
 		assert.Nil(err)
 		return float64(i), nil
 	}
-	matches := func(accessor cells.EventSinkAccessor) cells.CriterionMatch {
-		ok, err := accessor.Match(func(index int, event cells.Event) (bool, error) {
-			avg, _ := event.Payload().GetFloat64(behaviors.PayloadEvaluationAverage)
-			return avg > 6.0, nil
-		})
-		assert.Nil(err)
-		if !ok {
-			return cells.CriterionDropLast
-		}
-		if accessor.Len() < 3 {
-			return cells.CriterionKeep
-		}
-		return cells.CriterionDone
+	stopper := func(cell cells.Cell, event cells.Event) error {
+		// TODO 2017-06-03 Mue Find criterion for stopping.
 	}
 	topics := []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"}
-	waiter := cells.NewPayloadWaiter()
 
-	env.StartCell("evaluator", behaviors.NewEvaluatorBehavior(evaluate))
-	env.StartCell("combo", behaviors.NewComboBehavior(matches))
-	env.StartCell("waiter", behaviors.NewWaiterBehavior(waiter))
-	env.Subscribe("evaluator", "combo")
-	env.Subscribe("combo", "waiter")
+	env.StartCell("evaluator", behaviors.NewEvaluatorBehavior(evaluator))
+	env.StartCell("stopper", behaviors.NewSimpleBehavior(stopper))
+	env.Subscribe("evaluator", "stopper")
 
 	go func() {
 		for i := 0; i < 10000; i++ {
@@ -71,20 +55,6 @@ func TestEvaluatorBehavior(t *testing.T) {
 			env.EmitNew(ctx, "evaluator", topic, nil)
 		}
 	}()
-
-	waitCtx, cancel := context.WithTimeout(ctx, time.Second)
-	defer cancel()
-	payload, err := waiter.Wait(waitCtx)
-	assert.Nil(err)
-	accessor, ok := payload.Get(behaviors.PayloadComboEvents, nil).(cells.EventSinkAccessor)
-	assert.True(ok)
-	assert.Length(accessor, 3)
-	accessor.Match(func(index int, event cells.Event) (bool, error) {
-		avg := event.Payload().GetFloat64(behaviors.PayloadEvaluationAverage, 0.0)
-		assert.True(avg > 6.0)
-		assert.Logf("avg: %f", avg)
-		return false, nil
-	})
 }
 
 // EOF
