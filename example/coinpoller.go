8e24001bfe5a4b9331fb0cc9496b8a3b6373ef52
Frank Mueller <mue@tideland.biz>
2017-07-29 23:45:41 +0200

    Prepared fetching the raw coin data

--- example/coinpoller.go
+++ example/coinpoller.go
@@ -32,6 +32,28 @@ const (
 )
 
 //--------------------
+// COIN
+//--------------------
+
+// RawCoin contains one raw coin object of the API.
+type RawCoin struct {
+	ID               string `json:"id"`
+	Name             string `json:"name"`
+	Symbol           string `json:"symbol"`
+	Rank             string `json:"rank"`
+	PriceUSD         string `json:"price_usd"`
+	PriceBTC         string `json:"price_btc"`
+	Volume24hUSD     string `json:"24h_volume_usd"`
+	MarketCapUSD     string `json:"market_cap_usd"`
+	AvailableSupply  string `json:"available_supply"`
+	TotalSupply      string `json:"total_supply"`
+	PercentChange1h  string `json:"percent_change_1h"`
+	PercentChange24h string `json:"percent_change_24h"`
+	PercentChange7d  string `json:"percent_change_7d"`
+	LastUpdated      string `json:"last_updated"`
+}
+
+//--------------------
 // COIN POLLER
 //--------------------
 
