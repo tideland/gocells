ae6bdea4297c448a7fed38da7ac953e2350584b5
Frank Mueller <mue@tideland.biz>
2017-08-08 12:58:20 +0200

    Made coin entry point a select filter

--- example/behaviors/coin.go
+++ example/behaviors/coin.go
@@ -25,11 +25,22 @@ import (
 // for a behavior running as entry point for operations on individual
 // coins.
 func MakeCoinEntryPoint() cells.Behavior {
+	mkSelectFilter := func(coinID string) func(cells.Event) (bool, error) {
+		return func(event cells.Event) (bool, error) {
+			var coin Coin
+			if err := event.Payload().Unmarshal(&coin); err != nil {
+				return false, err
+			}
+			return coin.ID == coinID, nil
+		}
+	}
 	processor := func(cell cells.Cell, event cells.Event) error {
 		coinID := event.Payload().String()
 		cellID := identifier.JoinedIdentifier("coin", coinID)
 		if !cell.Environment().HasCell(cellID) {
-			cell.Environment().StartCell(cellID, behaviors.NewBroadcasterBehavior())
+			cell.Environment().StartCell(cellID, behaviors.NewSelectFilterBehavior(mkSelectFilter(coinID)))
+			cell.Environment().Subscribe("coins-splitter", cellID)
+			cell.Environment().Subscribe(cellID, "logger")
 		}
 		return nil
 	}
